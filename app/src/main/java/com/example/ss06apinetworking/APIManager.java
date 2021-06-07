6fc1941da8455c6a57f256fdabc443d93cff9566
ntdat1597 <53183723+ntdat1597@users.noreply.github.com>
2021-06-07 11:44:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ss06apinetworking/APIManager.java
@@ -0,0 +1,17 @@
+package com.example.ss06apinetworking;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface APIManager {
+    String SERVER_URL = "https://api-demo-anhth.herokuapp.com/";
+
+    @GET("data.json")
+    Call<Item>getItemData();
+    //Call<Item> getItemData(@Query("page") int page,@Query("limit") String limit);
+    @GET("datas.json")
+    Call<Item>getListData();
+
+
+}
